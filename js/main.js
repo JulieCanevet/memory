46a8acc982ff631d78b5c1026778c2553f7bffa5
Julie.Canevet <julie.canevet@hotmail.fr>
2017-07-19 11:05:36 +0200

    ajout de gagne

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-var couleur = ["red", "red", "yellow", "yellow", "blue", "blue", "green", "green", "black", "black", "pink", "pink", "purple", "purple"];
+var couleur = ["#D5D7CA", "#D5D7CA", "#D7D36C", "#D7D36C", "#ADA915", "#ADA915", "#486855", "#486855", "#4B9295", "#4B9295", "#79BBBA", "#79BBBA", "#EEE731", "#EEE731"];
 var rand = [];
 var allImg = document.getElementsByClassName("carte");
 var back = [];
+var gagne = [];
 console.log(allImg);
   
 // Repartition aleatoire des cartes
@@ -25,18 +26,28 @@ for (let j=0; j < allImg.length; j++){
 function decouvrir(){
   console.log(this);
   this.style.opacity = "1";
-  back.push(this.style.backgroundColor);
+  back.push(this);
   console.log(back);
   
-    if((back.length == 2) && (back[0]!= back[1])){
+  // Comparer et retourner si les cartes sont différentes
+    if((back.length == 2) && (back[0].style.backgroundColor != back[1].style.backgroundColor)){
+      console.log("good");
+      setTimeout(function(){
+        back[0].style.opacity = "0";
+        back[1].style.opacity = "0";
+        back = [];
+  }, 800);
 
-alert("recommence");
-back = [];
-    }
-    else if((back.length == 2) && back[0]== back[1]){
-        alert('BARVO');
-back = [];
-
+        
+    } 
+    // Laisser l'opacité si les cartes sont pareilles 
+    else if((back.length == 2) && back[0].style.backgroundColor == back[1].style.backgroundColor){
+        back = [];
+        gagne.push(this);
+        console.log("gagne" + gagne.length);
+        if (gagne.length == 7) {
+          alert('bravo');
+        }
 
     }
 }    
